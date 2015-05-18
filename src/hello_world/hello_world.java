a5ec6d2ee6c0693bf8673d8745ba4b5a03b38f12
user <user@114.71.78.169>
2015-05-18 19:47:22 +0900

    viva korea 수정

--- src/hello_world/hello_world.java
+++ src/hello_world/hello_world.java
@@ -3,6 +3,6 @@ package hello_world;
 public class hello_world {
 	public static void main(String[] args){
 		System.out.println("hello world");
-		System.out.println("hello korea");
+		System.out.println("viva korea");
 	}
 }
