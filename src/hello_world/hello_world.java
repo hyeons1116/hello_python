64d9ef6cc3a841deb4f9a27f69dd77c55ed98563
user <user@114.71.78.169>
2015-05-18 20:01:13 +0900

    핳

--- src/hello_world/hello_world.java
+++ src/hello_world/hello_world.java
@@ -4,5 +4,6 @@ public class hello_world {
 	public static void main(String[] args){
 		System.out.println("hello world");
 		System.out.println("viva korea");
+		System.out.println("hello eartt");
 	}
 }
